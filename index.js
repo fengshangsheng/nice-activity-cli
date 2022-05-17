ac04342aa591037d2f61d118356ae3a39c5c803e
fengshangsheng <1820959467@qq.com>
2022-05-17 12:00:10 +0800

    添加option、command指令；拆分模块

--- index.js
+++ index.js
@@ -2,25 +2,17 @@
 
 const program = require('commander');
 const packageJSON = require('./package.json');
+const initOption = require('./lib/core/option');
+const initCommand = require('./lib/core/command')
 
 // 查看版本号
 program.version(packageJSON.version, '-v, --vesion', '输出版本号');
 
-// 添加自己options，可通过--help查看
-program.option('-a --addzz <filepath>', '在指定目标路径创建文件目录; 例：nice -add /src/compoents/home')
-  .hook('preAction', (thisCommand, actionCommand) => {
-    console.log('hook-----------------');
-  })
-  .action((name, options, command) => {
-    console.log('action---------------', name);
-  })
-  .on('option:addzz', function (filepath) {
-    console.log('option:filepath------------', filepath);
-  });
+// 初始化指令
+initOption();
+initCommand();
 
-
-program.parse();
-
+program.parse(process.argv);
 
 const options = program.opts();
-console.log('========2=====options', options);
+console.log('====================================================');
