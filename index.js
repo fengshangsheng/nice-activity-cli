662e0cc0220c0901e1303ca04ce0a11e1b3e0d24
fengshangsheng <1820959467@qq.com>
2022-05-17 18:51:25 +0800

    添加create指令

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const packageJSON = require('./package.json');
 const initOption = require('./lib/core/option');
 const initCommand = require('./lib/core/command')
 
-// 查看版本号
-program.version(packageJSON.version, '-v, --vesion', '输出版本号');
 
 // 初始化指令
 initOption();
@@ -15,4 +13,3 @@ initCommand();
 program.parse(process.argv);
 
 const options = program.opts();
-console.log('====================================================');
