060c630470a854dd7b93e0918b1f218de1db3891
fengshangsheng <1820959467@qq.com>
2022-05-19 23:57:20 +0800

    test

--- utils/uitl.js
+++ utils/uitl.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const fs = require('fs');
+const fs = require('node:fs/promises');
 const ejs = require('ejs');
 
 const absolutePath = path.resolve(__dirname, '..');
@@ -8,51 +8,53 @@ const absolutePath = path.resolve(__dirname, '..');
 const isWindowEnv = () => {
   return process.platform === 'win32'
 }
-
-// 编译模板
-const compileTemplate = (templateType) => {
-  const regExp = new RegExp(`^${templateType}-component`);
+// 获取ejs编译模板
+const getEjsTemplate = (dirName) => {
+  const regExp = new RegExp(`^${dirName}`);
   const tplDir = path.resolve(absolutePath, './template');
   const files = matchDir(tplDir, regExp, true);
 
   return files;
 }
-
-// 匹配目录中的文件
+// 匹配文件
 const matchFile = (rootDirPath, regExp, deep = false) => {
-  const init = (dirPath) => {
-    const matchFile = {};
-    const names = fs.readdirSync(dirPath);
-    const fileList = names.filter((item) => {
+  const init = async (dirPath) => {
+    const matchFile = [];
+    const names = await fs.readdir(dirPath);
+    const fileList = names.filter(async (item) => {
       const itemPath = path.resolve(dirPath, item);
-      const stats = fs.statSync(itemPath);
+      const stats = await fs.stat(itemPath);
       return stats.isFile();
     });
-    const dirList = names.filter((item) => {
+    const dirList = names.filter(async (item) => {
       const itemPath = path.resolve(dirPath, item);
-      const stats = fs.statSync(itemPath);
+      const stats = await fs.stat(itemPath);
       return stats.isDirectory();
     });
 
-    fileList.forEach((item) => {
+    fileList.forEach(async (item) => {
       const itemPath = path.resolve(dirPath, item);
       const isMatch = regExp.test(item);
       if (isMatch) {
-        matchFile[item] = fs.readFileSync(itemPath)
+        matchFile.push({
+          absolutePath: itemPath,
+          fileBuffer: await fs.readFile(itemPath),
+          filename: item
+        })
       }
     })
 
     if (deep) {
-      dirList.forEach((item) => {
+      dirList.forEach(async (item) => {
         const itemPath = path.resolve(dirPath, item);
-        matchFile[item] = init(itemPath);
+        matchFile.push(...await init(itemPath));
       })
     }
     return matchFile;
   }
   return init(rootDirPath);
 }
-// 匹配目录中的子目录中的文件
+// 匹配特定目录下的所有文件
 const matchDir = (rootDirPath, regExp, deep) => {
   const init = (dirPath) => {
     const names = fs.readdirSync(dirPath);
@@ -69,18 +71,26 @@ const matchDir = (rootDirPath, regExp, deep) => {
       const itemPath = path.resolve(dirPath, matchDir);
       return matchFile(itemPath, /\.*/g, true);
     } else if (deep) {
-      return dirList.map((item) => {
-        const itemPath = path.resolve(dirPath, item);
-        return init(itemPath);
-      })
+      return dirList
+        .map((item) => {
+          const itemPath = path.resolve(dirPath, item);
+          return init(itemPath)
+        })
+        .flat(Infinity);
     } else {
-      return {}
+      return []
     }
   }
   return init(rootDirPath);
 }
+// 写入文件
+const writeFile = (fileObj, templateString) => {
+  const existDir = fs.readdirSync(process.cwd());
+  console.log('data', data);
+}
 
 module.exports = {
   isWindowEnv,
-  compileTemplate
+  getEjsTemplate,
+  writeFile
 }
