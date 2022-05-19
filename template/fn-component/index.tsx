f5c0d8c183c364aa358d63a57648feef0039e8f4
fengshangsheng <1820959467@qq.com>
2022-05-19 14:36:12 +0800

    添加匹配文件目录工具函数

--- template/fn-component/index.tsx
+++ template/fn-component/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import './App.css';
 
-function App() {
+function Index() {
   const [count, handleCount] = useState(0);
   return (
     <div className="App">
@@ -10,4 +10,4 @@ function App() {
   );
 }
 
-export default App;
+export default Index;
--- utils/uitl.js
+++ utils/uitl.js
@@ -11,54 +11,72 @@ const isWindowEnv = () => {
 
 // 编译模板
 const compileTemplate = (templateType) => {
-  const template = getTemplate(templateType);
-  console.log('template', template);
+  const regExp = new RegExp(`^${templateType}-component`);
+  const tplDir = path.resolve(absolutePath, './template');
+  const files = matchDir(tplDir, regExp, true);
+
 }
 
-const getTemplate = (type) => {
-  const templateDir = path.resolve(absolutePath, './template');
+// 匹配目录中的文件
+const matchFile = (rootDirPath, regExp, deep = false) => {
+  const init = (dirPath) => {
+    const matchFile = {};
+    const names = fs.readdirSync(dirPath);
+    const fileList = names.filter((item) => {
+      const itemPath = path.resolve(dirPath, item);
+      const stats = fs.statSync(itemPath);
+      return stats.isFile();
+    });
+    const dirList = names.filter((item) => {
+      const itemPath = path.resolve(dirPath, item);
+      const stats = fs.statSync(itemPath);
+      return stats.isDirectory();
+    });
 
-  dirMatchFile(templateDir, /.tsx$/g, true);
-
-  // const matchEjs = require.context('./template', true, /[\w-]+\.ejs$/);
-  // matchEjs.keys().forEach((fileName, index) => {
-  //   console.log('fileName', fileName);
-  //   if (fileName.indexOf(fileName) === 0) {
-  //     const componentConfig = matchEjs(fileName)
-  //     return componentConfig.default;
-  //   }
-  // })
-  // return false;
-}
-
-const dirMatchFile = (dirPath, regExp, deep = false) => {
-  const matched = [];
-
-  function init(dirPath, regExp, deep) {
-    fs.readdir(dirPath, async (er, fileNames) => {
-      if (er) throw new Error(er);
-
-      const file = [];
-      for (let i = 0; i < fileNames.length; i++) {
-        const item = fileNames[i];
-        const itemPath = path.resolve(dirPath, item);
-        const stats = fs.lstatSync(itemPath);
-
-        if (stats.isFile() && regExp.test(item)) {
-          const zz = await fs.readFileSync(itemPath)
-          console.log('zz', zz);
-          file.push(zz);
-        }
-        if (deep && stats.isDirectory()) {
-          file.push(...init(itemPath, regExp, deep));
-        }
+    fileList.forEach((item) => {
+      const itemPath = path.resolve(dirPath, item);
+      const isMatch = regExp.test(item);
+      if (isMatch) {
+        matchFile[item] = fs.readFileSync(itemPath)
       }
-      console.log('file', file);
-      return file;
     })
-  }
 
-  return init(dirPath, regExp, deep);
+    if (deep) {
+      dirList.forEach((item) => {
+        const itemPath = path.resolve(dirPath, item);
+        matchFile[item] = init(itemPath);
+      })
+    }
+    return matchFile;
+  }
+  return init(rootDirPath);
+}
+// 匹配目录中的子目录中的文件
+const matchDir = (rootDirPath, regExp, deep) => {
+  const init = (dirPath) => {
+    const names = fs.readdirSync(dirPath);
+    const dirList = names.filter((item) => {
+      const itemPath = path.resolve(dirPath, item);
+      const stats = fs.statSync(itemPath);
+      return stats.isDirectory();
+    });
+
+    const matchDir = dirList.find((item) => regExp.test(item));
+
+    if (matchDir) {
+      // 获取目录下所有文件
+      const itemPath = path.resolve(dirPath, matchDir);
+      return matchFile(itemPath, /\.*/g, true);
+    } else if (deep) {
+      return dirList.map((item) => {
+        const itemPath = path.resolve(dirPath, item);
+        return init(itemPath);
+      })
+    } else {
+      return {}
+    }
+  }
+  return init(rootDirPath);
 }
 
 module.exports = {
